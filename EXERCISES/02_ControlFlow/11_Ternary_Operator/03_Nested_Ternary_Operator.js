f373513da397e5ceb9b9984ec576a3ebc2043e4c
ET GLap <eglapointe@hotmail.com>
2024-08-22 12:51:24 -0400

    fix: variable value typo

--- EXERCISES/02_ControlFlow/11_Ternary_Operator/03_Nested_Ternary_Operator.js
+++ EXERCISES/02_ControlFlow/11_Ternary_Operator/03_Nested_Ternary_Operator.js
@@ -2,17 +2,17 @@
 
 // Step 1: Declare two variables, 
     // the first variable should be named: isWeekend with the value of false
-    // the second variable should be named: time with the value of 'morning'
+    // the second variable should be named: time with the value of 'Morning'
 // Write your code below
 
 
 
 
-// Step 2: Use nested ternary operators to set a third variable named: 'greeting' to
-    // 'Enjoy your weekend' if isWeekend is true,
-    // otherwise, use a second condition to check if time is 'morning',
-        // set it to 'Good morning' if it's true,
-        // and 'Good day' if it's false.
+// Step 2: Use nested ternary operators to set a third variable named: 'Greeting' to
+    // 'Enjoy your Weekend!' if isWeekend is true,
+    // otherwise, use a second condition to check if time is 'Morning',
+        // set it to 'Good Morning!' if it's true,
+        // and 'Good Day!' if it's false.
 
     // Hint: Ternary operators are similar to if-else statements but written in a shorter way: 
         // variable = (condition) ? value1 if true : (new condition) ? value2 if true : value3 if false
@@ -23,7 +23,7 @@
 
 // Don't change the code of the console.log()
 console.log(greeting);
-// Expected Output: Good morning
+// Expected Output: Good Morning!
 
 
 
--- __tests__/EXERCISES/02_ControlFlow/11_Ternary_Operator/03_Nested_Ternary_Operator.test.js
+++ __tests__/EXERCISES/02_ControlFlow/11_Ternary_Operator/03_Nested_Ternary_Operator.test.js
@@ -2,6 +2,6 @@ const { execSync } = require('child_process');
 
 test('Check nested ternary operator for isWeekend and time', () => {
   const output = execSync('node EXERCISES/02_ControlFlow/11_Ternary_Operator/03_Nested_Ternary_Operator.js').toString().trim();
-  const expectedOutput = 'Good morning';
+  const expectedOutput = 'Good Morning!';
   expect(output).toBe(expectedOutput);
 });
