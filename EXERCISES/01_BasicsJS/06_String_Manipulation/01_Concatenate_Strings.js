4871cce5864a36adb8ff32d12da5d0b4f3a4079d
ET GLap <eglapointe@hotmail.com>
2024-08-22 10:54:48 -0400

    fix: instruction with right value for test.

--- EXERCISES/01_BasicsJS/06_String_Manipulation/01_Concatenate_Strings.js
+++ EXERCISES/01_BasicsJS/06_String_Manipulation/01_Concatenate_Strings.js
@@ -1,8 +1,8 @@
 // Exercise 1.6.1
 
 // Step 1: Declare two variables,
-    // 'firstName' with the value of your firstname
-    // 'lastName' with the value of your lastname
+    // 'firstName' with the value of 'John'
+    // 'lastName' with the value of 'Doe'
 // Write your code below
 
 
@@ -15,8 +15,8 @@
 
 
 // Don't change the code of the console.log()
-console.log(`My first name is ${firstName} and my last name is ${lastName}, I am ${fullName}`);
-// Example of Expected Output: My first name is Alice and my last name is Smith, I am Alice Smith
+console.log(`My first name is ${firstName} and my last name is ${lastName}, I am ${fullName}.`);
+// Example of Expected Output: My first name is John and my last name is Doe, I am John Doe.
 
 
 
